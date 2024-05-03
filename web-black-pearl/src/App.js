7ce2fb66ba473f78d3cafebf9a9722660c85ecaf
gurujerseyno7 <bgmnov6@gmail.com>
2024-05-03 14:45:54 +0530

    Push notification

--- web-black-pearl/src/App.js
+++ web-black-pearl/src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Map from "./pages/Map";
+import { Box } from "@mui/material";
 
 const App = () => {
   return (
-    <Router>
-      {" "}
-      {/* Wrap everything with Router */}
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/map" element={<Map />} />
-      </Routes>
-    </Router>
+    <Box>
+      <Router>
+        {" "}
+        {/* Wrap everything with Router */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </Router>
+    </Box>
   );
 };
 
